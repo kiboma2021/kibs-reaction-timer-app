9d6049997bbc614e38bdcb8deb7c99781eebab43
kiboma <omayioben@gmail.com>
2023-02-22 15:17:48 +0300

    Rank scores

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,10 +1,28 @@
 <template>
   <h1> You took {{ score }} ms to complete the game</h1>
+  <h1>{{ this.rank }}</h1>
 </template>
 
 <script>
 export default {
-    props: ['score']
+    props: ['score'],
+
+    data() {
+        return {
+            rank: null,
+        }
+    },
+    mounted() {
+        if(this.score < 700){
+            this.rank="Well done. You scored among the first percentile"
+        }
+        else if (this.score <1200){
+            this.rank="Good work in getting to the 2nd percentile. You can better"
+        }
+        else {
+            this.rank="Too slow. Please improve !!!"
+        }
+    }
 
 }
 </script>
