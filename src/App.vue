236d7a879f8940158c92e251290dc88a04d2acb9
kiboma <omayioben@gmail.com>
2023-02-22 14:59:59 +0300

    Display score

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,10 @@
   <button @click="startPlaying" :disabled="isPlaying">Play</button>
 </div>
 
+<h1 v-if="showTimeTaken"> You took {{ this.score }} ms to complete the game</h1>
+
 <div v-if="isPlaying">
-  <Block :delay="delay">
+  <Block :delay="delay" @end="endGame">
     
   </Block>
 </div>
@@ -24,13 +26,20 @@ export default {
       title: "Welcome to Kibs Reaction Response Game",
       isPlaying: false,
       delay: null,
+      score: null,
+      showTimeTaken: false,
     }
   },
   methods: {
     startPlaying(){
-      this.delay = 2000 + Math.random() *5000
+      this.delay= 2000 + Math.random()*5000
       this.isPlaying = true
-    }
+    },
+    endGame(timeTaken){
+      this.isPlaying = false,
+      this.score = timeTaken,
+      this.showTimeTaken = true
+    },
   }
 }
 </script>
